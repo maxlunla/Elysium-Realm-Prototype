b656b4ea9e11a2bda968e04ad3e5363cbd6709d3
Thitikorn Padungwatsawat <60070018@kmitl.ac.th>
2025-09-12 03:37:18 +0700

    Anti-Hero: Add switch mechanic
     - Add switch for interact to close the light

---
+++ Assets/Script/Switch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+	[Header("Objects to Hide")]
+	public GameObject[] objectsToHide; // วัตถุที่จะหายไป
+
+	public GameObject interactText;
+
+	private bool playerInTrigger = false;
+
+	void Update()
+	{
+		if (playerInTrigger && Input.GetKeyDown(KeyCode.F))
+		{
+			ActivateSwitch();
+		}
+	}
+
+	void ActivateSwitch()
+	{
+		foreach (GameObject obj in objectsToHide)
+		{
+			obj.SetActive(false); // ทำให้วัตถุหายไป
+		}
+	}
+
+	// ตรวจว่า Player อยู่ใน Trigger
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerInTrigger = true;
+			interactText.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerInTrigger = false;
+			interactText.SetActive(false);
+		}
+	}
+}
